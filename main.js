b992059e43fff5389e34e8172e691367a5aad6cb
Andrew Hollingworth <andrewhollingworth@Andrews-MacBook-Air.local>
2019-08-11 18:07:30 -0400

    Small formatting edits to CSS. Some javascript additions

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ============PROGRESS BAR=============//
 // https://www.w3schools.com/howto/howto_js_scroll_indicator.asp
 function progressBar() {
   const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
